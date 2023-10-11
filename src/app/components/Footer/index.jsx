5294be1cb7bae27b4f5647139ab30de451d68eff
Daniel Cerverizzo <daniel.cerverizzo@cobrefacil.com.br>
2023-10-10 23:46:13 -0300

    feat: first commit on waterlife project

---
+++ src/app/components/Footer/index.jsx
@@ -0,0 +1,27 @@
+"use client"
+
+import './style.css';
+
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+      <footer>
+        <div className="opening-hours">
+          <p>
+            ⏰Nosso horário de funcionamento:⏰ De segunda a sexta das 09h as 18h, sábado das 09h as 17h
+          </p>
+        </div>
+        <div className="company">
+          @WATERLIFE AQUARISMO 2023
+        </div>
+        <div className="social-media">
+          Instagram
+          Wpp
+          Facebook
+        </div>
+      </footer>
+    </div>
+  )
+}
---
+++ src/app/components/Header/index.jsx
@@ -0,0 +1,20 @@
+"use client"
+
+import './style.css';
+
+import React from 'react';
+
+export default function Header() {
+    return (
+      <div>
+        <div className="nav">
+          <div class="logo">Waterlife</div>
+          <div class="nav-item">Promoções</div>
+          <div class="nav-item">Sobre Nós</div>
+          <div class="nav-item">Carrinho</div>
+        </div>
+      </div>
+    );
+  }
+
+
